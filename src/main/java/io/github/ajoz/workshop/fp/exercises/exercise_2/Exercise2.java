da208d0b9b8b7978203079b3814e8ab2995ae418
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2018-11-16 23:31:02 +0100

    exercise 2, java and kotlin version

---
+++ src/main/java/io/github/ajoz/workshop/fp/exercises/exercise_2/Exercise2.java
@@ -0,0 +1,27 @@
+package io.github.ajoz.workshop.fp.exercises.exercise_2;
+
+/*
+  Create a class that can represents a one argument function from a generic type
+  A to a generic type B
+
+  - part 1:
+  Express the function from String to String that is adding word "foo" to every
+  argument. Use it to concatenate: "bar", "baz", "qux"
+
+  - part 2:
+  Use the new Function type to express the functions from the first exercise:
+  1) y = x + 1
+  2) y = (x + 2)^2
+  3) y = -x + 10
+
+  - part 3:
+  Create functions:
+  1) f that takes a String and converts it to an Integer
+  2) g that takes an Integer and converts it to a Boolean - true if it is 42, false
+     otherwise
+  3) h that takes a String and returns a Boolean - h should do what a combination
+     of f and g would, you can use f and g to solve this.
+
+  Is there a problem with function f?
+  Can we combine f and g in a meaningful way?
+ */
