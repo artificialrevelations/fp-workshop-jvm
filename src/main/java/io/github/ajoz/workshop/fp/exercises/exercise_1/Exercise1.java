cdce9fb09f2f32fe1b46e2f1393f5f6d832695d7
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2018-11-16 23:31:31 +0100

    exercise 1, typo fix

--- src/main/java/io/github/ajoz/workshop/fp/exercises/exercise_1/Exercise1.java
+++ src/main/java/io/github/ajoz/workshop/fp/exercises/exercise_1/Exercise1.java
@@ -5,7 +5,7 @@ package io.github.ajoz.workshop.fp.exercises.exercise_1;
 
   Express the function y = x + 1 with your class.
   Express the function y = (x + 2)^2 with your class.
-  Express the function y = -x + 100 with you class.
+  Express the function y = -x + 10 with your class.
 
   Expressing only simple Integer to Integer functions does not look very powerful
   and useful can this Function definition be enhanced to work on any type?
