18179e276721801c7c516f582b7bc84e23205006
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2018-11-18 23:43:24 +0100

    exercise 5, java exercise, solution, tests

---
+++ src/test/java/io/github/ajoz/workshop/fp/part_1/solutions/exercise_5/Exercise5Test.java
@@ -0,0 +1,38 @@
+package io.github.ajoz.workshop.fp.part_1.solutions.exercise_5;
+
+import static io.github.ajoz.workshop.fp.part_1.solutions.exercise_5.Exercise5.composeConsumer;
+import static io.github.ajoz.workshop.fp.part_1.solutions.exercise_5.Exercise5.composeSupplier;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Exercise5Test {
+    @Test
+    public void should_compose_function_with_consumer() {
+        // given:
+        final Consumer1<Integer> consumer =
+                integer -> System.out.println(String.format("consumed: %d", integer));
+
+        final Function1<String, Integer> function = Integer::valueOf;
+
+        // when:
+        final Consumer1<String> tested = composeConsumer(function, consumer);
+
+        // then:
+        tested.accept("42");
+    }
+
+    @Test
+    public void should_compose_function_with_supplier() {
+        // given:
+        final Supplier<String> supplier =
+                () -> "42";
+
+        final Function1<String, Integer> function = Integer::valueOf;
+
+        // when:
+        final Supplier<Integer> tested = composeSupplier(function, supplier);
+
+        // then:
+        Assert.assertEquals(new Integer(42), tested.get());
+    }
+}
