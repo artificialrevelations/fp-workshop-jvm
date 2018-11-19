5faccb2c2e3fb580a933967e9ad91e76e4e439df
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2018-11-19 13:57:57 +0100

    composition practice, java, kotlin, solutions and tests

---
+++ src/main/kotlin/io/github/ajoz/workshop/fp/part_1/solutions/practice/PraKtice.kt
@@ -0,0 +1,13 @@
+@file:Suppress("PackageName")
+
+package io.github.ajoz.workshop.fp.part_1.solutions.practice
+
+// foo
+infix fun <A> ((A) -> Boolean).and(other: (A) -> Boolean): (A) -> Boolean {
+    return { value -> this(value) && other(value) }
+}
+
+// bar
+infix fun <A> ((A) -> Boolean).or(other: (A) -> Boolean): (A) -> Boolean {
+    return { value -> this(value) || other(value) }
+}
