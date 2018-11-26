a8077740a0627049dee96c4c46e6d619af6489be
Łukasz Tokarski <garencjusz@gmail.com>
2018-11-26 09:32:41 +0100

    Finished Exercise1 in Java. *** Cherry picked from Lukasz's Gitlab ***

--- src/main/java/io/github/ajoz/workshop/fp/java/part_1/exercises/exercise_1/Exercise1.java
+++ src/main/java/io/github/ajoz/workshop/fp/java/part_1/exercises/exercise_1/Exercise1.java
@@ -13,3 +13,38 @@ package io.github.ajoz.workshop.fp.java.part_1.exercises.exercise_1;
   and useful can this Function definition be enhanced to work on any type?
  */
 
+interface Function {
+    Integer invoke(Integer input);
+}
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+        // region task1
+        Function f = new Function() {
+            @Override
+            public Integer invoke(Integer input) {
+                return input + 1;
+            }
+        };
+
+        Function lambdaF = input -> input + 1;
+        printFunction(2, lambdaF);
+        // endregion
+
+        // region task2
+        Function lambdaG = input -> (input + 2) * (input +2);
+        printFunction(3, lambdaG);
+        // endregion
+
+        // region task3
+        Function h = input -> -input + 10;
+        printFunction(4, h);
+        // endregion
+    }
+
+    private static void printFunction(final Integer argument,
+                                      final Function function) {
+        System.out.println(String.format("f(%d) = %d ", argument, function.invoke(argument)));
+    }
+}
