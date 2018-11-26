d91283bf1af23fff34033ce4106b5aa8e2376aea
Łukasz Tokarski <garencjusz@gmail.com>
2018-11-26 09:33:28 +0100

    Finished Exercise2 in Java. *** Cherry picked from Lukasz's Gitlab ***

--- src/main/java/io/github/ajoz/workshop/fp/java/part_1/exercises/exercise_2/Exercise2.java
+++ src/main/java/io/github/ajoz/workshop/fp/java/part_1/exercises/exercise_2/Exercise2.java
@@ -27,3 +27,55 @@ package io.github.ajoz.workshop.fp.java.part_1.exercises.exercise_2;
   Is there a problem with function f?
   Can we combine f and g in a meaningful way?
  */
+
+interface Function<S, R> {
+    R invoke(S input);
+}
+
+public class Exercise2 {
+    public static void main(String[] args) {
+
+        // region part1
+        Function<String, String> str2str = input -> input + "foo";
+        printFunction("bar", str2str);
+        printFunction("baz", str2str);
+        printFunction("qux", str2str);
+        // endregion
+
+        // region part 2
+        Function f = new Function<Integer, Integer>() {
+            @Override
+            public Integer invoke(Integer input) {
+                return input + 1;
+            }
+        };
+
+        Function<Integer, Integer> lambdaF = input -> input + 1;
+        printFunction(2, lambdaF);
+
+        Function<Integer, Integer> lambdaG = input -> (input + 2) * (input +2);
+        printFunction(3, lambdaG);
+
+        Function<Integer, Integer> h = input -> -input + 10;
+        printFunction(4, h);
+        // endregion
+
+        // region part 3
+        Function<String, Integer> f1 = Integer::valueOf;
+        printFunction("31", f1);
+
+        Function<Integer, Boolean> g = input -> input == 42;
+        printFunction(42, g);
+        printFunction(43, g);
+
+        Function<String, Boolean> h1 = (String input) -> g.invoke(f1.invoke(input));
+        printFunction("43", h1);
+        printFunction("42", h1);
+        // endregion
+    }
+
+    private static <S, R> void printFunction(final S argument,
+                                             final Function<S, R> function) {
+        System.out.println(String.format("f(%s) = %s ", argument, function.invoke(argument)));
+    }
+}
