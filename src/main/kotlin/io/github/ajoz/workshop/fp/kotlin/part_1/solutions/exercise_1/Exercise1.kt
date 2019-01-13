fd98f665172618f3c4e12ac3ef7c9f4cdb9bd5ae
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-01-13 23:42:16 +0100

    [Kotlin][Part 1][Exercise 1] Reworking of Exercise 1 in Kotlin, solutions, tests

--- src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/part_1/solutions/exercise_1/Exercise1.kt
+++ src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/part_1/solutions/exercise_1/Exercise1.kt
@@ -2,50 +2,23 @@
 
 package io.github.ajoz.workshop.fp.kotlin.part_1.solutions.exercise_1
 
-// We can express a one argument function in the form of an abstract class:
-abstract class KFunction1 {
-    // what will happen if we will add operator keyword?
-    abstract fun invoke(argument: Int): Int
-}
+// Part 1:
+// y = x + 1
+val f1: (Int) -> Int = { x: Int -> x + 1 }
 
-// We can express a one argument function in the form of an interface:
+// y = (x + 2)^2
+val f2: (Int) -> Int = { x -> (x + 2) * (x + 2) }
+
+// y = -x + 10
+val f3: (Int) -> Int = { x -> -x + 10 }
+
+// y = x^2 + 4x + 1
+val f4: (Int) -> Int = { x -> x * x + 4 * x + 1 }
+
 /*
-interface KFunction1 {
-    fun invoke(argument: Int): Int
-}
-*/
+  Part 2:
 
-fun main(args: Array<String>) {
-    val xPlusOne: KFunction1 = object : KFunction1() {
-        // although the argument in our Kotlin version is called
-        // "argument" we change its name to "x" so its more familiar
-        // beware that this conflicts with Kotlin named arguments feature
-        override fun invoke(x: Int): Int = x + 1
-    }
+  Create a function from String to Int that returns the size of a given String.
+ */
 
-    val xPlusTwoSquared = object : KFunction1() {
-        override fun invoke(x: Int) = (x + 2) * (x + 2)
-    }
-
-    val minusXPlusTen = object : KFunction1() {
-        override fun invoke(x: Int) = -x + 10
-    }
-
-    printFunction(xPlusOne, 1)
-    printFunction(xPlusOne, 2)
-
-    printFunction(xPlusTwoSquared, 3)
-    printFunction(xPlusTwoSquared, 4)
-
-    printFunction(minusXPlusTen, 5)
-    printFunction(minusXPlusTen, 6)
-
-//    Check how does Kotlin behave with changed names of arguments:
-//    println("test of named argument: ${xPlusOne.invoke(argument = 42)}")
-//    println("test of named argument: ${xPlusOne.invoke(x = 42)}")
-}
-
-fun printFunction(function: KFunction1,
-                  argument: Int) {
-    println("kx = $argument ky = ${function.invoke(argument)}")
-}
+val strlen: (String) -> Int = { str -> str.length }
