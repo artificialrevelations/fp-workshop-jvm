6a4251eff72d7f812b60d0ab3288dd717ea3ce01
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-01-15 23:58:18 +0100

    [Java][Part 1][Exercise 2] Streamlining the exercise, making it more approachable

--- src/main/java/io/github/ajoz/workshop/fp/java/part_1/exercises/exercise_2/Exercise2.java
+++ src/main/java/io/github/ajoz/workshop/fp/java/part_1/exercises/exercise_2/Exercise2.java
@@ -3,27 +3,61 @@ package io.github.ajoz.workshop.fp.java.part_1.exercises.exercise_2;
 /*
   -- Advanced Functions --
 
-  Create a class that can represents a one argument function from a generic type
-  A to a generic type B
+  In the previous exercise we create a simple Integer to Integer function. This
+  kind of type was very limiting because we had to create a function type for
+  each pair of types we wanted to create an association.
 
-  - part 1:
-  Express the function from String to String that is adding word "foo" to every
-  argument. Use it to concatenate: "bar", "baz", "qux"
+  To achieve something more flexible we need generics. We can rework our
+  previous example
 
-  - part 2:
-  Use the new Function type to express the functions from the first exercise:
-  1) y = x + 1
-  2) y = (x + 2)^2
-  3) y = -x + 10
+  interface IntegerFunction {
+      Integer apply(Integer argument);
+  }
 
-  - part 3:
-  Create functions:
-  1) f that takes a String and converts it to an Integer
-  2) g that takes an Integer and converts it to a Boolean - true if it is 42, false
-     otherwise
-  3) h that takes a String and returns a Boolean - h should do what a combination
-     of f and g would, you can use f and g to solve this.
-
-  Is there a problem with function f?
-  Can we combine f and g in a meaningful way?
+  We need a separate type for the argument and a separate type for the result:
  */
+interface Function1<A, B> {
+    B apply(A argument);
+}
+
+class Exercise2 {
+    /*
+      Part 1:
+
+      Change the functions below so that they are using our new Function1 type.
+     */
+    static final IntegerFunction f1 = x -> x + 1;
+    static final StringToIntegerFunction f2 = str -> str.length();
+    static final StringFunction f3 = str -> str + "foo";
+
+    /*
+      Part 2:
+
+      Create 3 functions:
+      - str2int that takes a String and converts it to an Integer
+      - int2bool that takes an Integer and converts it to a Boolean:
+        * it should return true if int is equal to 42
+        * it should return false otherwise
+      - str2bool that takes a String and returns a Boolean:
+        * this function should do what a combination of str2int and int2bool would
+        * you can use str2int and int2bool to solve this exercise
+     */
+
+    
+
+    public static void main(final String[] args) {
+
+    }
+}
+
+interface StringToIntegerFunction {
+    Integer apply(String argument);
+}
+
+interface StringFunction {
+    String apply(String argument);
+}
+
+interface IntegerFunction {
+    Integer apply(Integer argument);
+}
