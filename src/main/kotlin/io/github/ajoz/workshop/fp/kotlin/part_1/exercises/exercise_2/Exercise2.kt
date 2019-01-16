2155a09014b8f23dc9dd5e85c3bcfe6de4efe376
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-01-16 23:13:36 +0100

    [Kotlin][Part 1][Exercise 2] Exercise, Solution and unit tests

--- src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/part_1/exercises/exercise_2/Exercise2.kt
+++ src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/part_1/exercises/exercise_2/Exercise2.kt
@@ -3,27 +3,68 @@
 package io.github.ajoz.workshop.fp.kotlin.part_1.exercises.exercise_2
 
 /*
-  Create a class that can represents a one argument function from a generic type
-  A to a generic type B
+  -- Advanced Functions --
 
-  - part 1:
-  Express the function from String to String that is adding word "foo" to every
-  argument. Use it to concatenate: "bar", "baz", "qux"
+  In the previous example we played a bit with some simple (Int) -> Int functions.
+  We even created a strlen function that was a (String) -> Int.
 
-  - part 2:
-  Use the new Function type to express the functions from the first exercise:
-  1) y = x + 1
-  2) y = (x + 2)^2
-  3) y = -x + 10
+  A type that can represent a function from some generic type A to some generic
+  type B can look like:
 
-  - part 3:
-  Create functions:
-  1) f that takes a String and converts it to an Integer
-  2) g that takes an Integer and converts it to a Boolean - true if it is 42, false
-     otherwise
-  3) h that takes a String and returns a Boolean - h should do what a combination
-     of f and g would, you can use f and g to solve this.
+  interface KFunction1<in A, out B> {
+    fun invoke(argument: A): B
+  }
 
-  Is there a problem with function f?
-  Can we combine f and g in a meaningful way?
+  We do not have to create such type on our own, Kotlin covers us here and its
+  possible to specify a function (A) -> B that is referring to generic types.
+
+  fun <A, B> foo(f: (A) -> B) {
+    // some code
+  }
+
+  This will be very useful when we will start to enhance the type with some
+  useful extension functions.
+
+  Part 1:
+
+  Please create 3 functions:
+  - f1 that takes an Int and returns an Int, this function should increment its
+    argument by one.
+  - f2 that takes a String and returns an Int, this function should return the
+    length of the given argument.
+  - f3 that takes a String and returns a String, this function should prepend
+    text "foo" to the given argument.
+
+  Uncomment the tests!
  */
+
+// val f1: (???) -> ??? = ???
+// val f2: (???) -> ??? = ???
+// val f3: (???) -> ??? = ???
+
+/*
+  Part 2:
+
+  Create 3 functions:
+  - str2int that takes a String and converts it to an Int
+  - int2bool that takes an Int and converts it to a Boolean:
+    * it should return true if int is equal to 42
+    * it should return false otherwise
+  - str2bool that takes a String and returns a Boolean:
+    * this function should do what a combination of str2int and int2bool would
+    * you can use str2int and int2bool to solve this exercise
+
+  Uncomment tests for this part!
+
+  Hints:
+  - to convert String to an Integer please use Integer.valueOf(String)
+
+  Questions:
+  - Can we generify the composition of two functions somehow?
+  - What types should the functions have?
+  - Is there a problem with the function str2int?
+ */
+
+// val str2int: (???) -> ??? = ???
+// val int2bool: (???) -> ??? = ???
+// val str2bool: (???) -> ??? = ???
--- src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/part_1/solutions/exercise_2/Exercise2.kt
+++ src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/part_1/solutions/exercise_2/Exercise2.kt
@@ -2,53 +2,12 @@
 
 package io.github.ajoz.workshop.fp.kotlin.part_1.solutions.exercise_2
 
-internal interface KFunction1<A, B> {
-    operator fun invoke(argument: A): B
-}
+// Part 1:
+val f1: (Int) -> Int = { it + 1 }
+val f2: (String) -> Int = { it.length }
+val f3: (String) -> String = { "foo$it" }
 
-fun main(args: Array<String>) {
-    val function = object : KFunction1<String, String> {
-        override fun invoke(argument: String) = "${argument}foo"
-    }
-
-    printFunction("bar", function)
-    printFunction("baz", function)
-    printFunction("qux", function)
-
-    // Let's use the default function type in Kotlin and with it all the
-    // nice syntactic sugar we have available
-    val xPlusOne: (Int) -> Int = { x -> x + 1 }
-    val xPlusTwoSquared: (Int) -> Int = { x -> (x + 2) * (x + 2) }
-
-    printKotlinFunction(1, xPlusOne)
-    printKotlinFunction(3, xPlusTwoSquared)
-
-    // instead of writing: val minusXPlusTen: (Int) -> Int = { x -> (-x) + 10 }
-    // we can use some sugar here:
-    printKotlinFunction(5) { x ->
-        (-x) + 10
-    }
-
-    // for easier reading we will drop f and g as a name
-    // and use something more descriptive
-    val stringToInt: (String) -> Int = { Integer.valueOf(it) }
-    val intToBool: (Int) -> Boolean = { it == 42 }
-
-    val stringToBool: (String) -> Boolean = { string ->
-        val result = stringToInt(string)
-        intToBool(result)
-    }
-
-    printKotlinFunction("41", stringToBool)
-    printKotlinFunction("42", stringToBool)
-}
-
-private fun <A, B> printFunction(argument: A,
-                                 function: KFunction1<A, B>) {
-    println(String.format("kx: A = %s ky: B = %s", argument, function(argument)))
-}
-
-private fun <A, B> printKotlinFunction(argument: A,
-                                       function: (A) -> B) {
-    println(String.format("> kx: A = %s ky: B = %s", argument, function(argument)))
-}
+// Part 2:
+val str2int: (String) -> Int = { Integer.valueOf(it) }
+val int2bool: (Int) -> Boolean = { it == 42 }
+val str2bool: (String) -> Boolean = { int2bool(str2int(it)) }
