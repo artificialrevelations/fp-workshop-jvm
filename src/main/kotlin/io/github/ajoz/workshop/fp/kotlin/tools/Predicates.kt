ea9e4ff897af47341055f28417aa9793e78cc4a7
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-02-06 23:22:55 +0100

    [Java/Kotlin][Tools] Adding missing Predicate related functions

--- src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/tools/Predicates.kt
+++ src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/tools/Predicates.kt
@@ -1,12 +1,17 @@
 package io.github.ajoz.workshop.fp.kotlin.tools
 
-// foo
 infix fun <A> ((A) -> Boolean).and(other: (A) -> Boolean): (A) -> Boolean =
         { value -> this(value) && other(value) }
 
-// bar
 infix fun <A> ((A) -> Boolean).or(other: (A) -> Boolean): (A) -> Boolean =
         { value -> this(value) || other(value) }
 
 fun <A> ((A) -> Boolean).not(): (A) -> Boolean =
         { value -> !this(value) }
+
+infix fun <A> ((A) -> Boolean).xor(other: (A) -> Boolean): (A) -> Boolean =
+        { value ->
+            val p = this(value)
+            val q = other(value)
+            (p || q) && !(p && q)
+        }
