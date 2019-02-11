e381bbf976519b3104e16f367dcdc1fde71980c0
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-02-12 00:29:25 +0100

    [Kotlin][Part 1][Practice 2] Exercises and solutions

---
+++ src/main/kotlin/io/github/ajoz/workshop/fp/kotlin/part_1/solutions/practice_2/Predicates.kt
@@ -0,0 +1,29 @@
+@file:Suppress("UNUSED_PARAMETER", "unused")
+
+package io.github.ajoz.workshop.fp.kotlin.part_1.solutions.practice_2
+
+import io.github.ajoz.workshop.fp.kotlin.tools.and
+import io.github.ajoz.workshop.fp.kotlin.tools.or
+
+// Part 1:
+fun <A : Any, B : Any> instanceOf(type: Class<out A>): (B) -> Boolean =
+        { type.isAssignableFrom(it::class.java) }
+
+// Part 2:
+fun <A> alwaysTrue(): (A) -> Boolean = { true }
+
+// Part 3:
+fun <A> alwaysFalse(): (A) -> Boolean = { false }
+
+// Part 4:
+fun <A> isEqualTo(value: A): (A) -> Boolean = { it == value }
+
+// Part 5:
+fun isLargerThen(value: Int): (Int) -> Boolean = { it > value }
+
+// Part 6:
+fun isLowerThen(value: Int): (Int) -> Boolean = { it < value }
+
+// Part 7:
+val isAllowed: (Int) -> Boolean =
+        (isLargerThen(0) and isLowerThen(6)) or isEqualTo(42)
