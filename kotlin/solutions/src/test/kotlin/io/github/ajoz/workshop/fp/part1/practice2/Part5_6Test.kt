76d5575bdf227a231148c3653eb7597d0725540f
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-02-23 19:50:37 +0100

    [dev] Using teoria library - Kotlin changes

--- kotlin/solutions/src/test/kotlin/io/github/ajoz/workshop/fp/part1/practice2/Part5_6Test.kt
+++ kotlin/solutions/src/test/kotlin/io/github/ajoz/workshop/fp/part1/practice2/Part5_6Test.kt
@@ -2,8 +2,8 @@
 
 package io.github.ajoz.workshop.fp.part1.practice2
 
-import io.github.ajoz.workshop.fp.tests.theory.Below
-import io.github.ajoz.workshop.fp.tests.theory.Above
+import io.teoria.junit.numbers.Above
+import io.teoria.junit.numbers.Below
 import org.junit.experimental.theories.Theories
 import org.junit.experimental.theories.Theory
 import org.junit.runner.RunWith
